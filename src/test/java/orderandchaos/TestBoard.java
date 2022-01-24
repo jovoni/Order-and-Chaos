d1395db32c663a3b65ec2597a466aabfda7daa15
elena-buscaroli <ele.buscaroli@gmail.com>
2022-01-24 17:27:43 +0100

    Board test class modified

--- src/test/java/orderandchaos/TestBoard.java
+++ src/test/java/orderandchaos/TestBoard.java
@@ -4,21 +4,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestBoard {
+    private final Board board = new Board();
 
     @Test
     void checkDimension() {
-        Board board = new Board();
         int expected = 36;
         assertEquals(board.size(), expected);
     }
 
     @Test
     void checkPiece(){
-        Board board = new Board();
         Position position = new Position(4, 4);
         board.getCellAt(position).placePiece(Piece.X);
         assertEquals(board.getCellAt(position).getPiece(), Piece.X);
-
     }
 
 }
