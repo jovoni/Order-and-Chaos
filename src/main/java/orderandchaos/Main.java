46a6d77507ff12722a200ea8961f9a7c7944d439
benedettaliberatori <benny.libera@gmail.com>
2022-01-25 17:41:48 +0100

    main added

--- src/main/java/orderandchaos/Main.java
+++ src/main/java/orderandchaos/Main.java
@@ -1,3 +1,25 @@
+package orderandchaos;
+
+import orderandchaos.Exceptions.NonIntegerException;
+import orderandchaos.Exceptions.NonValidPieceException;
+import orderandchaos.Exceptions.NonValidPosException;
+
 public class Main {
+
+    public static void main(String[] args) throws NonValidPosException, NonIntegerException, Cell.PosAlreadyOccupiedException, NonValidPieceException {
+
+        Game game = new Game();
+        Display display = new Display(game.board);
+        Position position = game.AskPosition();
+        Piece piece = game.AskPiece();
+        game.MakeMove(position, piece);
+
+        display.PrintBoard();
+
+
+
+
+    }
+
     
 }
