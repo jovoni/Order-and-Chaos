8a42205785394cf86243afb05e0203097e421aef
benedettaliberatori <benny.libera@gmail.com>
2022-01-25 17:45:11 +0100

    display changed

--- src/main/java/orderandchaos/Display.java
+++ src/main/java/orderandchaos/Display.java
@@ -8,14 +8,14 @@ public class Display {
     }
     public void PrintBoard(){
         System.out.println();
-        for (int i = 6; i>0; i--){
+        for (int i = 1; i<=6; i++){
             for(int j = 1; j <= 6; j++){
                 Cell cell = board.getCellAt(new Position(i,j));
                 if(!cell.isOccupied()){
                     System.out.print("[]");
                 }
                 else{
-                    System.out.print(cell.getPiece());
+                    System.out.print("[" + cell.getPiece() + "]");
                 }
             }
             System.out.println();
