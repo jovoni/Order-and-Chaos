5d8f4a240a4cb85df635acc763c0274a198566a9
elena-buscaroli <ele.buscaroli@gmail.com>
2022-01-27 15:26:30 +0100

    Updating Main

--- src/main/java/orderandchaos/Main.java
+++ src/main/java/orderandchaos/Main.java
@@ -1,16 +1,14 @@
 package orderandchaos;
 
-import orderandchaos.Exceptions.NonIntegerException;
 import orderandchaos.Exceptions.NonValidPieceException;
 import orderandchaos.Exceptions.NonValidPosException;
 
 public class Main {
 
-    public static void main(String[] args) throws NonValidPosException, NonIntegerException, Cell.PosAlreadyOccupiedException, NonValidPieceException {
+    public static void main(String[] args) throws NonValidPosException, Cell.PosAlreadyOccupiedException, NonValidPieceException {
 
         Game game = new Game();
         Display display = new Display(game.board);
-
 
         boolean orderHasWon = false;
         while (!orderHasWon && !game.board.isFull()) {
