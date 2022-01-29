993572e5071590b2738558c8e1791d2144bdad90
benedettaliberatori <benny.libera@gmail.com>
2022-01-29 10:50:51 +0100

    tested new Game methods

--- src/test/java/orderandchaos/TestBoard.java
+++ src/test/java/orderandchaos/TestBoard.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestBoard {
     private final Board board = new Board();
@@ -34,4 +37,34 @@ public class TestBoard {
         }
         assertEquals(full, board.isFull());
     }
-}
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    void checkgetRow(int i) throws Cell.PosAlreadyOccupiedException {
+
+        for (int j = 1; j <= 6; j++) {
+            board.getCellAt(new Position(i, j)).placePiece(Piece.X);
+
+            }
+
+        Set<Cell> row = board.getRow(new Position(i,1));
+        assertTrue(row.stream().allMatch(c->c.getPiece()==Piece.X));
+
+        }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    void checkgetCol(int i) throws Cell.PosAlreadyOccupiedException {
+
+        for (int j = 1; j <= 6; j++) {
+            board.getCellAt(new Position(j, i)).placePiece(Piece.O);
+
+        }
+
+        Set<Cell> col = board.getCol(new Position(1,i));
+        assertTrue(col.stream().allMatch(c->c.getPiece()==Piece.O));
+
+    }
+
+    }
+
