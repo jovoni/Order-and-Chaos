4ed20478769775d64f8425d692bdd44e13b10174
valerianilucrezia <72545549+valerianilucrezia@users.noreply.github.com>
2022-02-11 11:06:39 +0100

    add Game and Piece controller

--- src/main/java/gui/project/RootController.java
+++ src/main/java/gui/project/RootController.java
@@ -3,18 +3,18 @@ package gui.project;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import orderandchaos.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class RootController implements Initializable {
 
-
     @FXML private GridController gridController;
 
-
-    public String symbol;
-
+    private Game game;
+    private String symbol;
+    public Piece piece;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -22,16 +22,22 @@ public class RootController implements Initializable {
     }
 
     public void onMouseClickedCircle() {
+
         this.symbol = "/zero.jpg";
+        this.piece = piece.X;
     }
 
     public void onMouseClickedCross() {
+
         this.symbol = "/cross.png" ;
+        this.piece = piece.X;
     }
 
     public String getSymbol(){
         return symbol;
     }
+
+    public Game getGame(){ return game; }
 
 
 
