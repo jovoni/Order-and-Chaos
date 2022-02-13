3751ca483e80e76ccb5e769f0df9c6114ae27b97
ciroantoniomami <mamiciroantonio@gmail.com>
2022-02-13 15:43:18 +0100

    Added Test on Block and Win

--- src/test/java/orderandchaos/TestBlock.java
+++ src/test/java/orderandchaos/TestBlock.java
@@ -1,6 +1,8 @@
 package orderandchaos;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,27 +10,29 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestBlock {
     private final Game game = new Game();
 
-    @Test
-    void CheckBlockingRow(){
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    void CheckBlockingRow(int row){
 
         int BCSize = game.BlockChecker.size();
-        game.board.getCellAt(new Position(1,3)).placePiece(Piece.X);
-        game.BlockChecker.update(new Position(1,3));
-        game.board.getCellAt(new Position(1,4)).placePiece(Piece.O);
-        game.BlockChecker.update(new Position(1,4));
+        game.board.getCellAt(new Position(row,3)).placePiece(Piece.X);
+        game.BlockChecker.update(new Position(row,3));
+        game.board.getCellAt(new Position(row,4)).placePiece(Piece.O);
+        game.BlockChecker.update(new Position(row,4));
 
 
         assertEquals(game.BlockChecker.size(), (BCSize -1));
     }
 
-    @Test
-    void CheckBlockingCol(){
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    void CheckBlockingCol(int col){
 
         int BCSize = game.BlockChecker.size();
-        game.board.getCellAt(new Position(3,1)).placePiece(Piece.X);
-        game.BlockChecker.update(new Position(3,1));
-        game.board.getCellAt(new Position(4,1)).placePiece(Piece.O);
-        game.BlockChecker.update(new Position(4,1));
+        game.board.getCellAt(new Position(3,col)).placePiece(Piece.X);
+        game.BlockChecker.update(new Position(3,col));
+        game.board.getCellAt(new Position(4,col)).placePiece(Piece.O);
+        game.BlockChecker.update(new Position(4,col));
 
 
         assertEquals(game.BlockChecker.size(), (BCSize -1));
@@ -47,14 +51,15 @@ public class TestBlock {
         assertEquals(game.BlockChecker.size(), (BCSize -1));
     }
 
-    @Test
-    void CheckBlockingLastAndFirstEquals(){
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    void CheckBlockingLastAndFirstEquals(int col){
 
         int BCSize = game.BlockChecker.size();
-        game.board.getCellAt(new Position(1,1)).placePiece(Piece.X);
-        game.BlockChecker.update(new Position(1,1));
-        game.board.getCellAt(new Position(6,1)).placePiece(Piece.X);
-        game.BlockChecker.update(new Position(6,1));
+        game.board.getCellAt(new Position(1,col)).placePiece(Piece.X);
+        game.BlockChecker.update(new Position(1,col));
+        game.board.getCellAt(new Position(6,col)).placePiece(Piece.X);
+        game.BlockChecker.update(new Position(6,col));
 
         assertEquals(game.BlockChecker.size(), (BCSize -1));
     }
--- src/test/java/orderandchaos/TestGame.java
+++ src/test/java/orderandchaos/TestGame.java
@@ -1,6 +1,59 @@
 package orderandchaos;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestGame {
+
+    private final Game game = new Game();
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    void TestWinRow(int col){
+        game.board.getCol(new Position(1,col)).stream().limit(5).forEach(cell -> cell.placePiece(Piece.X));
+
+        assertTrue(new Win(game.board, new Position(1,col)).checkWin());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    void TestWinCol(int row){
+        game.board.getRow(new Position(row,1)).stream().limit(5).forEach(cell -> cell.placePiece(Piece.X));
+
+        assertTrue(new Win(game.board, new Position(row,1)).checkWin());
+    }
+
+    @Test
+    void TestWinDiag(){
+        game.board.getRow(new Position(1,1)).stream().limit(5).forEach(cell -> cell.placePiece(Piece.X));
+
+        assertTrue(new Win(game.board, new Position(1,1)).checkWin());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    void TestNotWinRow(int col){
+        game.board.getCol(new Position(1,col)).stream().limit(6).forEach(cell -> cell.placePiece(Piece.X));
+
+        assertFalse(new Win(game.board, new Position(1,col)).checkWin());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    void TestNotWinCol(int row){
+        game.board.getRow(new Position(row,1)).stream().limit(6).forEach(cell -> cell.placePiece(Piece.X));
+
+        assertFalse(new Win(game.board, new Position(row,1)).checkWin());
+    }
+
+
+
+
+
+
 
 }
 
